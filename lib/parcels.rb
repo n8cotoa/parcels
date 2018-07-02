160190af6524f1f9ca8a3dded15208b1b4bd890e
Nate Cottle and Reese Glasscock <nate@>
2018-07-02 10:42:24 -0700

    Add positive fail for first test.

---
+++ lib/parcels.rb
@@ -0,0 +1,11 @@
+class Parcel
+  def initialize(length, width, height)
+    @l = length
+    @w = width
+    @h = height
+  end
+
+  def volume()
+
+  end
+end
--- spec/parcels_spec.rb
+++ spec/parcels_spec.rb
@@ -3,5 +3,8 @@ require 'pry'
 require 'parcels'
 
 describe('#parcels') do
-  
+  it("Program will calculate the volume of the package") do
+    new_package = Parcel.new(2, 3, 4)
+    expect(new_package.volume).to(eq(24))
+  end
 end
