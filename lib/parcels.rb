fef798a731016bde41bf19368f73545e448f678d
Nate Cottle and Reese Glasscock <reese@>
2018-07-02 11:02:12 -0700

    Add second passing test.

--- lib/parcels.rb
+++ lib/parcels.rb
@@ -1,11 +1,19 @@
 class Parcel
+  attr_accessor(:volume)
   def initialize(length, width, height)
-    @l = length
-    @w = width
-    @h = height
+    @l = length.to_i
+    @w = width.to_i
+    @h = height.to_i
+    @volume = volume.to_i
   end
 
   def volume()
     @volume = @l * @w * @h
+  end
+
+  def cost_to_ship()
+    cost = 0
+    cost += @volume * 5
+    cost
   end
 end
--- spec/parcels_spec.rb
+++ spec/parcels_spec.rb
@@ -7,4 +7,8 @@ describe('#parcels') do
     new_package = Parcel.new(2, 3, 4)
     expect(new_package.volume).to(eq(24))
   end
+  it("Program will calculate cost to ship") do
+    new_package = Parcel.new(2, 3, 4)
+    expect(new_package.cost_to_ship).to(eq(120))
+  end
 end
